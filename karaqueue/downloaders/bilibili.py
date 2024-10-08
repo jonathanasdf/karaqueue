7226301fd25f9c4b848eeb31c56589f7c354f2c4
Jonathan Shen <jonathanasdf@gmail.com>
2024-10-08 15:00:22 -0700

    Update bilix

--- karaqueue/downloaders/bilibili.py
+++ karaqueue/downloaders/bilibili.py
@@ -2,13 +2,13 @@
 import asyncio
 import logging
 import os
+import pathlib
 from typing import List
-import bilix
-import bilix.api.bilibili
-import bilix.progress
+import bilix.progress.cli_progress
 import bilix.utils
 import discord
 import StringProgressBar
+from bilix.sites.bilibili import api, DownloaderBilibili
 
 from karaqueue import common
 from karaqueue import utils
@@ -21,7 +21,7 @@ _SESSDATA = 'SESSDATA'
 SESSDATA = common.CONFIG.get(_SECTION, _SESSDATA, fallback='')
 
 
-class Progress(bilix.progress.CLIProgress):
+class Progress(bilix.progress.cli_progress.CLIProgress):
     """Progress bar."""
 
     def __init__(self, entry: common.Entry, cancel: List[asyncio.Event]) -> None:
@@ -29,21 +29,21 @@ class Progress(bilix.progress.CLIProgress):
         self._cancel = cancel
         super().__init__()
 
-    async def add_task(self, *args, **kwargs):
-        return await super().add_task(*args, **kwargs)
-
     async def update(self, task_id, **kwargs):
         for event in self._cancel:
             if event.is_set():
                 raise asyncio.CancelledError()
         task = self.tasks[task_id]
         await super().update(task_id, **kwargs)
-        total_size_mb = task.total / 1024 / 1024
-        progress = StringProgressBar.progressBar.filledBar(
-            task.total, task.completed)  # type: ignore
-        self._entry.load_msg = (
-            f'Loading bilibili video `{self._entry.title}`...\n'
-            f'Downloading: {progress[0]} {progress[1]:0.0f}% of {total_size_mb:0.1f}Mb')
+        if task.total is None:
+            self._entry.load_msg = f'Loading bilibili video `{self._entry.title}`...'
+        else:
+            total_size_mb = task.total / 1024 / 1024
+            progress = StringProgressBar.progressBar.filledBar(
+                task.total, task.completed)  # type: ignore
+            self._entry.load_msg = (
+                f'Loading bilibili video `{self._entry.title}`...\n'
+                f'Downloading: {progress[0]} {progress[1]:0.0f}% of {total_size_mb:0.1f}Mb')
 
 
 class BilibiliDownloader(common.Downloader):
@@ -55,9 +55,7 @@ class BilibiliDownloader(common.Downloader):
     async def load(
         self, interaction: discord.Interaction, url: str, *, video: bool, audio: bool,
     ) -> common.DownloadResult:
-        informer = bilix.info.InformerBilibili(sess_data=SESSDATA)
-        info = await bilix.api.bilibili.get_video_info(informer.client, url)
-        await informer.aclose()
+        info = await api.get_video_info(DownloaderBilibili(sess_data=SESSDATA).client, url)
         if not info.dash:
             raise RuntimeError('Unknown error getting video.')
         if info.dash.duration == 0:
@@ -69,17 +67,15 @@ class BilibiliDownloader(common.Downloader):
         for quality in info.dash.video_formats:
             if len(info.dash.video_formats[quality]) == 0:
                 if '720P' in quality:
-                    logging.warning(f'Could not get video for {quality}, try updating SESSDATA.')
+                    logging.warning(
+                        f'Could not get video for {quality}, try updating SESSDATA.')
 
-        title = bilix.utils.legal_title(info.h1_title, info.pages[info.p].p_name)
+        title = bilix.utils.legal_title(info.title, info.pages[info.p].p_name)
         await utils.edit(interaction, content=f'Loading bilibili video `{title}`...')
 
         async def load_streams(entry: common.Entry, cancel: List[asyncio.Event]):
-            downloader = bilix.DownloaderBilibili(
-                videos_dir=entry.path, progress=Progress(entry, cancel),
-                sess_data=SESSDATA)
-            await downloader.get_video(url)
-            await downloader.aclose()
+            async with DownloaderBilibili(progress=Progress(entry, cancel), sess_data=SESSDATA) as downloader:
+                await downloader.get_video(url, path=pathlib.Path(entry.path))
             video_path = f'{title}.mp4'
 
             result = common.LoadResult()
